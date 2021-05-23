af28d07a1fedf46f17c333f74318b4e2e953737d
Liza Kops <55504714+liza2403@users.noreply.github.com>
2021-05-23 05:02:17 +0300

    Create data_base.py

---
+++ src/app/data_base.py
@@ -0,0 +1,18 @@
+import sqlite3
+def createTable():
+    connection = sqlite3("login.db")
+
+    connection.execute("CREATE TABLE USERS(USERNAME TEXT NOT NULL, EMAIL TEXT, PASSWORD TEXT)")
+    connection.execute("INSERT INTO USERS VALUES(?,?,?)",('liza', 'l24@gmail.com','abcde'))
+
+    connection.commit()
+    result= connection.execute("SELECT * FROM USERS")
+
+    for data in result:
+        print("Username", data [0])
+        print("Email", data [1])
+        print("Password", data [2])
+
+        connection.close()
+
+createTable()
